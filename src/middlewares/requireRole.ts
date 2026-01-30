a4172080db488fc28a227649e98cd177f824ce78
Subham Mondal <subhamoffi@gmail.com>
2026-01-30 19:08:21 +0530

    Added require role middleware and verifyuser middleware

---
+++ src/middlewares/requireRole.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+
+export const requireRole = (role: Role) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        if (req.userRole != role)
+            return res.status(403).json({
+                success: false,
+                data: null,
+                error: "FORBIDDEN",
+            });
+
+        next();
+    };
+};
